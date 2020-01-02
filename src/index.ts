889a436b20ae0331bf2736e5b301b9334f39df25
Dmitry Selikhov <selikhov.dmitrey@gmail.com>
2020-01-02 23:46:43 +0300

    fix: remove comments

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,1 @@
 export const Test = (name: string): string => `Hello ${name}`;
-
-// https://github.com/blakeembrey/change-case/blob/master/packages/lower-case/src/index.ts
-// https://github.com/nbubna/Case/blob/master/src/Case.js
