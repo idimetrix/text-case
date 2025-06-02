5667568841e9b150d34babe0f1475153a4812661
Dmitry Selikhov <selikhov.dmitrey@gmail.com>
2025-06-02 17:42:11 -0400

    Update packages

--- packages/constant-case/src/index.spec.ts
+++ packages/constant-case/src/index.spec.ts
@@ -41,7 +41,7 @@ const TEST_CASES: [string, string][] = [
   ["test-123", "TEST_123"],
   ["test.123", "TEST_123"],
   ["test 123", "TEST_123"],
-  ["123test", "123_TEST"],
+  ["123test", "123TEST"],
   ["123_test", "123_TEST"],
   ["version2.0", "VERSION2_0"],
   ["v2.0.1", "V2_0_1"],
@@ -104,7 +104,7 @@ const TEST_CASES: [string, string][] = [
   ["user+tag@example.com", "USER_TAG_EXAMPLE_COM"],
 
   // Extreme edge cases
-  ["a1bStar", "A1_B_STAR"],
+  ["a1bStar", "A1B_STAR"],
   ["ID123String", "ID123_STRING"],
   ["Id123String", "ID123_STRING"],
   ["XMLParser2", "XML_PARSER2"],
